fd5cb0085efa39d32c4ba0c2554f4b109a4447a4
Nikolai Magnussen <nikolai@magnussen.tf>
2019-01-20 03:09:36 +0100

    Add account creation and hashing using argon2

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 #![allow(proc_macro_derive_resolution_fallback)]
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
-#[macro_use] extern crate diesel;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate diesel;
 
-use rocket::request::{Form};
+use rocket::http::{Cookie, Cookies};
+use rocket::request::Form;
 use rocket::response::{NamedFile, Redirect};
-use rocket::http::{Cookies, Cookie};
-
 
 use std::path::Path;
 
-mod schema;
 mod models;
-use models::{User, Admin, Database};
+mod schema;
+use models::{Admin, Database, User};
 
 #[derive(FromForm, Debug)]
 struct UserLogin {
@@ -22,23 +24,24 @@ struct UserLogin {
     password: String,
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Heisann da"
+#[derive(FromForm, Debug)]
+struct UserCreation {
+    username: String,
+    password: String,
+    privileged: bool,
 }
 
-#[post("/login", data="<input>")]
-fn new(input: Form<UserLogin>, mut cookies: Cookies) -> Redirect {
-    let user_id = if input.username == "user" {
-        "0"
-    } else if input.username == "admin" {
-        "1"
-    } else {
-        "-1"
-    };
+#[get("/")]
+fn index() -> Redirect {
+    Redirect::to("/login")
+}
 
-    let cookie = Cookie::new("user_id", user_id);
-    cookies.add_private(cookie);
+#[post("/login", data = "<input>")]
+fn new(input: Form<UserLogin>, mut cookies: Cookies, conn: Database) -> Redirect {
+    if let Ok(user) = conn.login_user(&input.username, &input.password) {
+        let cookie = Cookie::new("user_id", user.id.to_string());
+        cookies.add_private(cookie);
+    }
 
     Redirect::to("/admin")
 }
@@ -54,17 +57,33 @@ fn login() -> Option<NamedFile> {
     NamedFile::open(Path::new("static/login.html")).ok()
 }
 
+#[get("/register")]
+fn register() -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/register.html")).ok()
+}
+
+#[post("/register", data = "<input>")]
+fn register_user(input: Form<UserCreation>, conn: Database) -> Redirect {
+    if let Ok(_) = conn.create_user(&input.username, &input.password, input.privileged) {
+        println!("Successfully created new user");
+    } else {
+        println!("Error creating new user");
+    }
+
+    Redirect::to("/login")
+}
+
 #[get("/admin")]
 fn admin_panel(admin: Admin) -> String {
     format!("Welcome in, administrator: {:?}", admin)
 }
 
-#[get("/admin", rank=2)]
+#[get("/admin", rank = 2)]
 fn admin_panel_user(user: User) -> String {
     format!("Sorry, admins only... {:?}", user)
 }
 
-#[get("/admin", rank=3)]
+#[get("/admin", rank = 3)]
 fn admin_panel_redirect() -> Redirect {
     Redirect::to("/login")
 }
@@ -72,6 +91,19 @@ fn admin_panel_redirect() -> Redirect {
 fn main() {
     rocket::ignite()
         .attach(Database::fairing())
-        .mount("/", routes![index, new, login, admin_panel, admin_panel_user, admin_panel_redirect, logout])
+        .mount(
+            "/",
+            routes![
+                index,
+                new,
+                login,
+                admin_panel,
+                admin_panel_user,
+                admin_panel_redirect,
+                logout,
+                register,
+                register_user
+            ],
+        )
         .launch();
 }
--- src/models.rs
+++ src/models.rs
@@ -1,16 +1,19 @@
-use rocket::request::{self, Request, FromRequest};
+use argonautica::{Hasher, Verifier};
+use rocket::request::{self, FromRequest, Request};
 
-use rocket_contrib::databases::diesel::{Queryable, SqliteConnection};
-use rocket::outcome::{IntoOutcome, Outcome};
-use diesel::{RunQueryDsl, QueryDsl};
+use diesel::prelude::*;
 use diesel::result::Error;
+use diesel::{QueryDsl, RunQueryDsl};
+use rocket::outcome::{IntoOutcome, Outcome};
+use rocket_contrib::databases::diesel::{Insertable, Queryable, QueryableByName, SqliteConnection};
 
-use crate::schema::users::dsl::users;
+use crate::schema::users;
 
 #[database("sqlite_db")]
 pub struct Database(SqliteConnection);
 
-#[derive(Queryable, Debug)]
+#[derive(Queryable, Debug, PartialEq, QueryableByName)]
+#[table_name = "users"]
 pub struct User {
     pub id: i32,
     pub username: String,
@@ -18,23 +21,75 @@ pub struct User {
     pub privileged: bool,
 }
 
-#[derive(Debug)]
-pub struct Admin{
-   pub user: User,
+#[derive(Insertable)]
+#[table_name = "users"]
+pub struct NewUser {
+    pub username: String,
+    pub password: String,
+    pub privileged: bool,
 }
+
+#[derive(Debug)]
+pub struct Admin {
+    pub user: User,
+}
+
+const HASHKEY: &'static str = "ARSTARST";
 
 impl Database {
     pub fn get_user(&self, id: i32) -> Result<User, Error> {
+        use crate::schema::users::dsl::users;
+
         users.find(id).first::<User>(&self.0)
+    }
+
+    pub fn create_user(
+        &self,
+        username: &str,
+        password: &str,
+        privileged: bool,
+    ) -> Result<usize, Error> {
+        use crate::schema::users::dsl::users;
+
+        let hash = Hasher::default().with_secret_key(HASHKEY).with_password(password).hash();
+        if let Ok(hash) = hash {
+            diesel::insert_into(users)
+                .values(NewUser {
+                    username: username.to_string(),
+                    password: hash,
+                    privileged,
+                })
+                .execute(&self.0)
+        } else {
+            Err(Error::__Nonexhaustive)
+        }
+    }
+
+    pub fn login_user(&self, username_prov: &str, password_prov: &str) -> Result<User, Error> {
+        use crate::schema::users::dsl::*;
+
+        let user: User = users.filter(username.eq(username_prov)).first(&self.0)?;
+        let verified = Verifier::default()
+            .with_secret_key(HASHKEY)
+            .with_hash(&user.password)
+            .with_password(password_prov)
+            .verify();
+
+        match verified {
+            Ok(true) => Ok(user),
+            Ok(false) => Err(Error::__Nonexhaustive),
+            Err(_) => Err(Error::__Nonexhaustive),
+        }
     }
 }
 
-impl <'a, 'r> FromRequest<'a, 'r> for User {
+impl<'a, 'r> FromRequest<'a, 'r> for User {
     type Error = ();
 
     fn from_request(request: &'a Request<'r>) -> request::Outcome<User, ()> {
         let db = request.guard::<Database>()?;
-        request.cookies()
+        request
+            .cookies()
             .get_private("user_id")
             .and_then(|cookie| cookie.value().parse().ok())
             .and_then(|id| db.get_user(id).ok())
@@ -42,7 +97,7 @@ impl <'a, 'r> FromRequest<'a, 'r> for User {
     }
 }
 
-impl <'a, 'r> FromRequest<'a, 'r> for Admin{
+impl<'a, 'r> FromRequest<'a, 'r> for Admin {
     type Error = ();
 
     fn from_request(request: &'a Request<'r>) -> request::Outcome<Admin, ()> {
